51b2eb33e2cc52941a60f6489bf1b599526edff1
Jeho Yeon <jehoyeon617@gmail.com>
2021-09-25 16:57:42 +0900

    dataBinding & Join Update

--- app/src/main/java/com/jeho/mysololife/auth/JoinActivity.kt
+++ app/src/main/java/com/jeho/mysololife/auth/JoinActivity.kt
@@ -1,37 +1,91 @@
 package com.jeho.mysololife.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import com.jeho.mysololife.MainActivity
 import com.jeho.mysololife.R
+import com.jeho.mysololife.databinding.ActivityJoinBinding
 
 class JoinActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
 
+    private lateinit var binding : ActivityJoinBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_join)
 
         auth = Firebase.auth
 
-//        auth.createUserWithEmailAndPassword("abc@abc.com", "abcdabcd")
-//            .addOnCompleteListener(this) { task ->
-//                if (task.isSuccessful) {
-//
-//                    Toast.makeText(this, "성공", Toast.LENGTH_LONG).show()
-//
-//                } else {
-//
-//                    Toast.makeText(this, "실패", Toast.LENGTH_LONG).show()
-//
-//                }
-//            }
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_join)
+
+        binding.joinBtn.setOnClickListener {
+
+            var isGoToJoin = true
+
+            val email = binding.emailArea.text.toString()
+            val password1 = binding.passwordArea1.text.toString()
+            val password2 = binding.passwordArea2.text.toString()
+
+            // by 제호 : 위의 값이 비어있는지 확인
+            if (email.isEmpty()) {
+                Toast.makeText(this, "이메일을 입력해주세요", Toast.LENGTH_LONG).show()
+                isGoToJoin = false
+            }
+
+            if (password1.isEmpty()) {
+                Toast.makeText(this, "password1을 입력해주세요", Toast.LENGTH_LONG).show()
+                isGoToJoin = false
+            }
+
+            if (password2.isEmpty()) {
+                Toast.makeText(this, "password2를 입력해주세요", Toast.LENGTH_LONG).show()
+                isGoToJoin = false
+            }
+
+            // by 제호 : 비밀번호가 같은지 확인
+            if (!password1.equals(password2)) {
+                Toast.makeText(this, "비밀번호를 똑같이 입력해주세요", Toast.LENGTH_LONG).show()
+                isGoToJoin = false
+            }
+
+            // by 제호 : 비밀번호가 6자리 이상인지 확인
+            if (password1.length < 6) {
+                Toast.makeText(this, "비밀번호를 6자리 이상 입력해주세요", Toast.LENGTH_LONG).show()
+                isGoToJoin = false
+            }
+
+            if (isGoToJoin) {
+
+                auth.createUserWithEmailAndPassword(email, password1).addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(this, "회원가입이 완료되었습니다", Toast.LENGTH_LONG).show()
+
+                            val intent = Intent(this, MainActivity::class.java)
+                            startActivity(intent)
+
+                        } else {
+                            Toast.makeText(this, "회원가입을 다시 진행해주세요", Toast.LENGTH_LONG).show()
+                        }
+
+                    }
+
+
+            }
+
+        }
+
+
+
+
 
     }
 }
