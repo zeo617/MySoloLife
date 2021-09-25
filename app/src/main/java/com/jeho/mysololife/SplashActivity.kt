d579facbc0ee416ec30728428c5c669fb158d0ba
Jeho Yeon <jehoyeon617@gmail.com>
2021-09-25 23:28:04 +0900

    #8 MainActivity Update

--- app/src/main/java/com/jeho/mysololife/SplashActivity.kt
+++ app/src/main/java/com/jeho/mysololife/SplashActivity.kt
@@ -4,19 +4,44 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.jeho.mysololife.auth.IntroActivity
 
 class SplashActivity : AppCompatActivity() {
 
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        auth = Firebase.auth
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        Handler().postDelayed({
-            startActivity(Intent(this, IntroActivity::class.java))
-            finish()
-        }, 3000)
+        if (auth.currentUser?.uid == null) {
+
+            Handler().postDelayed({
+                startActivity(Intent(this, IntroActivity::class.java))
+                finish()
+            }, 3000)
+
+            Log.d("SplashActivity", "null")
+
+        } else {
+
+            Handler().postDelayed({
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }, 3000)
+
+            Log.d("SplashActivity", "not null")
+
+        }
+
+
 
     }
 }
